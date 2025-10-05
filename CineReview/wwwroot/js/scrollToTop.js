76353dfd10a7481212b96650443f4e792c0b8e75
Kiet Tran <44864496+kiettran99@users.noreply.github.com>
2025-10-05 03:59:28 +0000

    Remove Blazor Project

--- CineReview/wwwroot/js/scrollToTop.js
+++
@@ -1,9 +0,0 @@
-window.history.scrollRestoration = 'manual';
-
-window.scrollToTopOnLoad = function () {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-};
-
-window.onload = function () {
-    window.scrollToTopOnLoad();
-};
